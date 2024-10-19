e0de56afdd753995523780230c92fc2703520be4
Dongjae Lee <league0415@naver.com>
2024-10-19 18:26:56 +0900

    전체적인 기본 프레임 생성 및 설정 완료

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,9 +1,19 @@
+import java.awt.BorderLayout;
+
 import javax.swing.JFrame;
 
 public class Calculator extends JFrame {
+	public Calculator() {
+		this.setTitle("怨꾩궛湲�");
+		this.setLayout(new BorderLayout());
+		this.setSize(400, 600);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setVisible(true);
+
+	}
 
 	public static void main(String[] args) {
-		System.out.println("중간고사 시작 테스트");
+		new Calculator();
 	}
 
 }
