8ef3627d118ca5b87a78892500da2603474476cb
Dongjae Lee <league0415@naver.com>
2024-10-21 23:33:54 +0900

    계산기 버튼 각각 생성

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,5 +1,8 @@
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.GridLayout;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
@@ -41,7 +44,31 @@ public class Calculator extends JFrame {
 	}
 
 	void showCenter() {
-		JPanel panel = new JPanel();
+		JPanel panel = new JPanel(); // 버튼 판넬
+		panel.setLayout(new GridLayout(5, 4, 3, 3));
+		panel.setBackground(Color.black);
+
+		// 계산기 연산버튼 각각 생성
+		JButton b1 = new JButton("CE");
+		JButton b2 = new JButton("C");
+		JButton b3 = new JButton("←");
+		JButton b4 = new JButton("÷");
+		JButton b5 = new JButton("7");
+		JButton b6 = new JButton("8");
+		JButton b7 = new JButton("9");
+		JButton b8 = new JButton("×");
+		JButton b9 = new JButton("4");
+		JButton b10 = new JButton("5");
+		JButton b11 = new JButton("6");
+		JButton b12 = new JButton("-");
+		JButton b13 = new JButton("1");
+		JButton b14 = new JButton("2");
+		JButton b15 = new JButton("3");
+		JButton b16 = new JButton("+");
+		JButton b17 = new JButton("±");
+		JButton b18 = new JButton("0");
+		JButton b19 = new JButton(".");
+		JButton b20 = new JButton("=");
 
 	}
 
