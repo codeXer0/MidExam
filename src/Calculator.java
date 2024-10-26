c22771083dd066392546ede23db8a95e3da55c34
Dongjae Lee <league0415@naver.com>
2024-10-27 00:47:47 +0900

    North 영역 panel 배치관리자 변경 및 주석 수정

--- src/Calculator.java
+++ src/Calculator.java
@@ -18,19 +18,27 @@ import javax.swing.JTextField;
  * 
  */
 public class Calculator extends JFrame {
-	// 각 연산버튼 변수 멤버필드로 선언
-	JButton b1, b2, b3, b4, b5, b6, b7, b8, b9, b10, b11, b12, b13, b14, b15, b16, b17, b18, b19, b20;
 	JTextField result;
 	JButton button;
-
-	// 연산버튼 생성을 위한 buttons 배열 생성
+	/**
+	 * 연산버튼 생성을 위한 buttons 배열 생성
+	 */
 	String[] buttons = { "CE", "C", "←", "/", "7", "8", "9", "*", "4", "5", "6", "-", "1", "2", "3", "+", "±", "0", ".",
 			"=" };
 
 	public Calculator() {
 		this.setTitle("계산기"); // "계산기"로 제목 설정
+		/**
+		 * 
+		 */
 		this.setLayout(new BorderLayout());
-		this.setSize(400, 600); // 가로 400, 세로 600픽셀 설정
+		/**
+		 * 사이즈 설정
+		 * 
+		 * @param 폭  400
+		 * @param 넓이 600
+		 */
+		this.setSize(400, 600); //
 
 		showNorth();
 		showCenter();
@@ -42,8 +50,16 @@ public class Calculator extends JFrame {
 	}
 
 	void showNorth() {
-		JPanel panel = new JPanel();
-		result = new JTextField("0.xxx", 38);
+		/**
+		 * North 영역 panel 배치관리자 변경
+		 * 
+		 * FlowLayout -> BorderLayout
+		 */
+		JPanel panel = new JPanel(new BorderLayout());
+		/**
+		 * JTextField 수정 가능여부 설정
+		 */
+		result = new JTextField("0.xxxxxxxxxxxx");
 		result.setEditable(false);
 		panel.add(result);
 		/**
@@ -58,21 +74,18 @@ public class Calculator extends JFrame {
 		JPanel panel = new JPanel();
 		// 5행 4열 좌우 7, 상하 간격 7인 그리드아웃을 panel에 생성
 		panel.setLayout(new GridLayout(5, 4, 7, 7));
-		// panel의 배경색을 black으로 변경
-		panel.setBackground(Color.black);
-
 		/**
-		 * buttons 배열과 for반복문을 사용하여 연산버튼을 생성
+		 * panel의 배경색을 black으로 변경
+		 */
+		panel.setBackground(Color.black);
+		/**
+		 * buttons 배열과 for반복문을 사용하여 연산버튼을 생성!!
+		 * 
+		 * 포커스 테두리 제거 및 패널 내 버튼 배치
 		 */
 		for (int i = 0; i < buttons.length; i++) {
 			JButton button = new JButton(buttons[i]);
-			/**
-			 * 포커스 테두리 제거
-			 */
 			button.setFocusPainted(false);
-			/**
-			 * 패널에 버튼 배치
-			 */
 			panel.add(button);
 		}
 		/**
@@ -84,20 +97,16 @@ public class Calculator extends JFrame {
 
 	void showSouth() {
 		/**
-		 * 남쪽영역 패널 생성
+		 * SOUTH 영역 패널 생성
 		 */
 		JPanel panel = new JPanel();
 		/**
-		 * 계산기록 버튼 생성
+		 * 계산기록 버튼 생성 및 포커스 테두리 제거
+		 * 
+		 * 패널 내 버튼 배치
 		 */
 		JButton historyButton = new JButton("History");
-		/**
-		 * 포커스 테두리 제거
-		 */
 		historyButton.setFocusPainted(false);
-		/**
-		 * 패널에 기록버튼 배치
-		 */ // 패널에 기록버튼 배치
 		panel.add(historyButton);
 
 		/**
